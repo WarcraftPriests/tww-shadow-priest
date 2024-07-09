4278da9d7b9bbb25b5d73ec55b05b7d967d1875e
Publik <sean.peters86@icloud.com>
2024-07-08 20:29:58 -0500

    add talents

--- talents/info.py
+++ talents/info.py
@@ -7,49 +7,6 @@ with open("../config.yml", "r", encoding="utf8") as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 
-info_talents = {
-    "whispering_shadows:1": [],
-    "mental_decay:1": [],
-    "mind_spike:1": [],
-    "maddening_touch:2": [],
-    "dark_evangelism:2": [],
-    "mind_devourer:2": [],
-    "phantasmal_pathogen:2": [],
-    "voidtouched:1": [],
-    "mindbender:1": [],
-    "inescapable_torment:1": [],
-    "mastermind:2": [],
-    "idol_of_yshaarj:1": [],
-    "deathspeaker:1": [],
-    "idol_of_nzoth:1": [],
-    "screams_of_the_void:2": [],
-    "auspicious_spirits:1": [],
-    "idol_of_yoggsaron:1": [],
-    "insidious_ire:2": [],
-}
-
-not_talents = {
-    "whispering_shadows:1": [],
-    "mental_decay:1": [],
-    "mind_spike:1": [],
-    "maddening_touch:2": [],
-    "dark_evangelism:2": [],
-    "mind_devourer:2": [],
-    "phantasmal_pathogen:2": [],
-    "voidtouched:1": [],
-    "mindbender:1": [],
-    "inescapable_torment:1": [],
-    "mastermind:2": [],
-    "idol_of_yshaarj:1": [],
-    "deathspeaker:1": [],
-    "idol_of_nzoth:1": [],
-    "screams_of_the_void:2": [],
-    "auspicious_spirits:1": [],
-    "idol_of_yoggsaron:1": [],
-    "insidious_ire:2": [],
-}
-
-
 def get_talents():
     talents = config["builds"]
     talents.pop("hero")
@@ -129,41 +86,83 @@ if __name__ == "__main__":
             file.close()
     print(f"Found {len(talent_spec_strings)} talent builds")
     # just do single target for now
-    with open("results/Results_Single.csv", "r", encoding="utf8") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if talent_spec_strings.get(row[1]):
-                talent_spec_strings[row[1]].dps = row[2]
-        # results_dict = {rows[1]:rows[2] for rows in reader}
-        file.close()
+    fights = ["2T", "3T", "4T", "Composite", "Dungeons-Slice", "Single"]
+    for fight in fights:
+        print(f"generating Results_{fight}-info.md")
+        with open(f"results/Results_{fight}.csv", "r", encoding="utf8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if talent_spec_strings.get(row[1]):
+                    talent_spec_strings[row[1]].dps = row[2]
+            file.close()
 
-    with open("results/info.md", "w", encoding="utf8") as file:
-        file.write("# Talent Info\n")
-        for build in talent_builds:
-            file.write(f"## {build}\n|talent|min|max|mean|\n|---|---|---|---|\n")
-            print(f"Getting stats for build: {build}")
-            # populate info_talents and not_talents
-            for talent in talent_spec_strings:
-                if not check_build_in_talent(build, talent):
-                    continue
-                talents = talent_spec_strings.get(talent)
-                spec_talents = talents.st
-                dps = talents.dps
+        with open(f"results/Results_{fight}-info.md", "w", encoding="utf8") as file:
+            file.write(f"# {fight} Talent Info\n")
+            for build in talent_builds:
+                info_talents = {
+                    "whispering_shadows:1": [],
+                    "mental_decay:1": [],
+                    "mind_spike:1": [],
+                    "maddening_touch:2": [],
+                    "dark_evangelism:2": [],
+                    "mind_devourer:2": [],
+                    "phantasmal_pathogen:2": [],
+                    "voidtouched:1": [],
+                    "mindbender:1": [],
+                    "inescapable_torment:1": [],
+                    "mastermind:2": [],
+                    "idol_of_yshaarj:1": [],
+                    "deathspeaker:1": [],
+                    "idol_of_nzoth:1": [],
+                    "screams_of_the_void:2": [],
+                    "auspicious_spirits:1": [],
+                    "idol_of_yoggsaron:1": [],
+                    "insidious_ire:2": [],
+                }
+
+                not_talents = {
+                    "whispering_shadows:1": [],
+                    "mental_decay:1": [],
+                    "mind_spike:1": [],
+                    "maddening_touch:2": [],
+                    "dark_evangelism:2": [],
+                    "mind_devourer:2": [],
+                    "phantasmal_pathogen:2": [],
+                    "voidtouched:1": [],
+                    "mindbender:1": [],
+                    "inescapable_torment:1": [],
+                    "mastermind:2": [],
+                    "idol_of_yshaarj:1": [],
+                    "deathspeaker:1": [],
+                    "idol_of_nzoth:1": [],
+                    "screams_of_the_void:2": [],
+                    "auspicious_spirits:1": [],
+                    "idol_of_yoggsaron:1": [],
+                    "insidious_ire:2": [],
+                }
+                file.write(f"## {build}\n|talent|min|max|mean|\n|---|---|---|---|\n")
+                # populate info_talents and not_talents
+                for talent in talent_spec_strings:
+                    if not check_build_in_talent(build, talent):
+                        continue
+                    talents = talent_spec_strings.get(talent)
+                    spec_talents = talents.st
+                    dps = talents.dps
+                    for t in info_talents:
+                        if t in spec_talents:
+                            info_talents[t].append(int(dps))
+                        else:
+                            not_talents[t].append(int(dps))
+                # print stats
                 for t in info_talents:
-                    if t in spec_talents:
-                        info_talents[t].append(int(dps))
-                    else:
-                        not_talents[t].append(int(dps))
-            # print stats
-            for t in info_talents:
-                if len(info_talents[t]) == 0:
-                    print(f"Build {build} never takes {t}, skipping")
-                    continue
-                if len(not_talents[t]) == 0:
-                    print(f"Build {build} always takes {t}, skipping")
-                    continue
-                min_change = get_change(min(info_talents[t]), min(not_talents[t]))
-                max_change = get_change(max(info_talents[t]), max(not_talents[t]))
-                mean_change = get_change(mean(info_talents[t]), mean(not_talents[t]))
-                file.write(f"|{t}|{min_change}%|{max_change}%|{mean_change}%\n")
-        exit()
+                    if len(info_talents[t]) == 0:
+                        print(f"Build {build} never takes {t}, skipping")
+                        continue
+                    if len(not_talents[t]) == 0:
+                        print(f"Build {build} always takes {t}, skipping")
+                        continue
+                    min_change = get_change(min(info_talents[t]), min(not_talents[t]))
+                    max_change = get_change(max(info_talents[t]), max(not_talents[t]))
+                    mean_change = get_change(mean(info_talents[t]), mean(not_talents[t]))
+                    file.write(f"|{t}|{min_change}%|{max_change}%|{mean_change}%\n")
+            file.close()
