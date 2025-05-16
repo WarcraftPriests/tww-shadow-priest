c0c3aea38a9257aff5a2854d86cb804d9e0d9270
Publik <sean.peters86@icloud.com>
2025-05-16 14:15:11 -0500

    trinket updates

--- trinket-combos/build_combos.py
+++ trinket-combos/build_combos.py
@@ -6,32 +6,32 @@ python build_combos.py
 from itertools import combinations
 
 combos = {
-    # s2 dungeons (672/684)
-    "Entropic_Skardyn_Core_672": "entropic_skardyn_core,id=219296,ilevel=672",
+    # s2 dungeons (671/684)
+    "Entropic_Skardyn_Core_671": "entropic_skardyn_core,id=219296,ilevel=671",
     "Entropic_Skardyn_Core_684": "entropic_skardyn_core,id=219296,ilevel=684",
-    "Signet_of_the_Priory_672": "signet_of_the_priory,id=219308,ilevel=672",
+    "Signet_of_the_Priory_671": "signet_of_the_priory,id=219308,ilevel=671",
     "Signet_of_the_Priory_684": "signet_of_the_priory,id=219308,ilevel=684",
-    "Synergistic_Brewterializer_672": "synergistic_brewterializer,id=219299,ilevel=672",
+    "Synergistic_Brewterializer_671": "synergistic_brewterializer,id=219299,ilevel=671",
     "Synergistic_Brewterializer_684": "synergistic_brewterializer,id=219299,ilevel=684",
-    "Carved_Blazikon_Wax_672": "carved_blazikon_wax,id=219305,ilevel=672",
+    "Carved_Blazikon_Wax_671": "carved_blazikon_wax,id=219305,ilevel=671",
     "Carved_Blazikon_Wax_684": "carved_blazikon_wax,id=219305,ilevel=684",
-    "Soulletting_Ruby_672": "soulletting_ruby,id=178809,ilevel=672",
+    "Soulletting_Ruby_671": "soulletting_ruby,id=178809,ilevel=671",
     "Soulletting_Ruby_684": "soulletting_ruby,id=178809,ilevel=684",
-    "Ingenious_Mana_Battery_672": "ingenious_mana_battery,id=169344,ilevel=672",
+    "Ingenious_Mana_Battery_671": "ingenious_mana_battery,id=169344,ilevel=671",
     "Ingenious_Mana_Battery_684": "ingenious_mana_battery,id=169344,ilevel=684",
-    # Liberation of Undermine (672/684)
+    # Liberation of Undermine (671/684)
     "Geargrinders_Spare_Keys_684": "geargrinders_spare_keys,id=230197,ilevel=684",
     "Reverb_Radio_684": "reverb_radio,id=230194,ilevel=684",
-    "House_of_Cards_672": "house_of_cards,id=230027,ilevel=672",
+    "House_of_Cards_671": "house_of_cards,id=230027,ilevel=671",
     "House_of_Cards_684": "house_of_cards,id=230027,ilevel=684",
-    "Mugs_Moxie_Jug_672": "mugs_moxie_jug,id=230192,ilevel=672",
+    "Mugs_Moxie_Jug_671": "mugs_moxie_jug,id=230192,ilevel=671",
     "Mugs_Moxie_Jug_684": "mugs_moxie_jug,id=230192,ilevel=684",
-    "Eye_of_Kezan_672": "eye_of_kezan,id=230198,ilevel=672",
+    "Eye_of_Kezan_671": "eye_of_kezan,id=230198,ilevel=671",
     "Eye_of_Kezan_684": "eye_of_kezan,id=230198,ilevel=684",
-    # delves (672)
-    "Quickwick_Candlestick_672": "quickwick_candlestick,id=225649,ilevel=672",
-    "Suspicious_Energy_Drink_672": "suspicious_energy_drink,id=235363,ilevel=672",
-    "Funhouse_Lens_672": "funhouse_lens,id=234217,ilevel=672",
+    # delves (671)
+    "Quickwick_Candlestick_671": "quickwick_candlestick,id=225649,ilevel=671",
+    "Suspicious_Energy_Drink_671": "suspicious_energy_drink,id=235363,ilevel=671",
+    "Funhouse_Lens_671": "funhouse_lens,id=234217,ilevel=671",
     # pvp (?)
     "Prized_Gladiators_Badge_of_Ferocity_658": "prized_gladiators_badge_of_ferocity,id=229780,ilevel=658",
 }
